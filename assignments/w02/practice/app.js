756796ed10df5c5022baad7b629c37aa61d785e9
Shifteon <bpwyatt04@gmail.com>
2022-01-14 22:10:33 -0700

    More practice and assignments

--- assignments/w02/practice/app.js
+++ assignments/w02/practice/app.js
@@ -1,21 +1,32 @@
+// Core modules
 const path = require('path');
 
+// NPM modules
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Express app
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// Our view engine
+app.set('view engine', 'ejs');
+
+// Custom routes
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop'); 
 
+// Parser and our public path
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+// use our routes
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
+// 404
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
+// Start the server
 app.listen(3000);
