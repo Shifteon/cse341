5ce97e78abe76c90868e54ab371b7f9c19a6c77c
Shifteon <bpwyatt04@gmail.com>
2022-01-19 10:20:42 -0700

    Store application

---
+++ assignments/online_shop/models/product.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        const p = path.join();
+    }
+
+    static fetchAll() {
+        return products;
+    }
+}
